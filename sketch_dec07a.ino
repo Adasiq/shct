be1d6513c1c24aa2dce40ceef166fc97211ce9b5
Anthony Adasik <anthonyadasiq@gmail.com>
2020-12-07 18:44:32 +0300

    some changes in setup

--- sketch_dec07a.ino
+++ sketch_dec07a.ino
@@ -1,9 +1,15 @@
-#define leftSensor 2 // левый датчик линии
+#define leftSensor 2       // левый датчик линии
+#define rightSensor 4      // правый датчик линии
+
 
 void setup() {
-  // put your setup code here, to run once:
+  // кривое определение мотора, но да пофик, я пагамист и я так вижу
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
+  //===========================================
+  //определяем пины для сенсоров линии
+  pinMode(leftSensor, INPUT);
+  pinMode(rightSensor, INPUT);
 }
 
 void loop() {
