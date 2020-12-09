b4a8a17f0b01335b39b49dd354d4054a414f8ec7
Anthony Adasik <anthonyadasiq@gmail.com>
2020-12-09 18:55:21 +0300

    initialization of robot added

--- sketch_dec07a.ino
+++ sketch_dec07a.ino
@@ -13,10 +13,39 @@ void setup() {
   //определяем пины для сенсоров линии
   pinMode(leftSensor, INPUT);
   pinMode(rightSensor, INPUT);
+  //=============================================
+  //индикация загрузки
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  // моргаем 10 раз встроенным светодиодом
+  for (size_t i = 0; i < 10; i++) {
+     digitalWrite(13, HIGH);
+     delay(100);
+     digitalWrite(13, LOW);
+     delay(100);
+  }
+  //проверяем все датчики
+  Serial.println("ezLineRobot ready to test");
+  //проверяем левый датчик
+  int leftTest = digitalRead(leftSensor);
+  Serial.print("left line sensor: ");
+  Serial.println(leftTest);
+  //проверяем правый датчик
+  int rightTest = digitalRead(rightSensor);
+  Serial.print("right line sensor: ");
+  Serial.println(rightTest);
+  //=============================================
+  //проверка моторов
+  //=============================================
+  Serial.println("motors testing...");
+  for (size_t i = 0; i < 255; i++) {
+     analogWrite(leftMotor, i);
+     analogWrite(rightMotor, i);
+     delay(10);
+  }
+  Serial.println("motors tested!")
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(11, HIGH);
-  digitalWrite(12, LOW);
+
 }
